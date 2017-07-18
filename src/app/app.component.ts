a729448b9c8f54da6ae62db7d19329d042c11f88
brebatet <benjamin.rebatet@soprasteria.com>
2017-07-18 21:32:45 +0200

    Fix game score

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,7 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Game', component: ListPage }
     ];
 
   }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, db: AngularFireDatabase) {
+    this.items = db.list('/players');
+  }
 
+  createNewGame() {
   }
 
 }
